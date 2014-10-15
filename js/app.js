c6b2d4fa2a463ef12dccef857533d30a678b8821
Jorge Romero <romeroqj@gmail.com>
2014-10-15 17:33:49 -0600

    add images, grid system, using single view

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
 var app = angular.module('app', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/',
-              {
-                  controller: 'CravingController',
-                  templateUrl: 'views/craving.html'
-              })
-        .when('/notokay',
-               {
-                   controller: 'CravingController',
-                   templateUrl: 'views/notokay.html'
-               })
-	.when('/okay',
-	      {
-		  controller: 'CravingController',
-		  templateUrl: 'views/okay.html'
-	      })
-        .otherwise({ redirectTo: '/' });
+  $routeProvider
+    .when('/',
+      {
+        controller: 'CravingController',
+        templateUrl: 'views/setup.html'
+      })
+    .otherwise({ redirectTo: '/' });
 }]);
 
--- js/controllers.js
+++ js/controllers.js
@@ -1,2 +1,3 @@
-app.controller('CravingController', function($scope) {
+app.controller('CravingController', function($scope) { 
+
 });
