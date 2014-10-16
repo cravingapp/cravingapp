7b1baa1d405e8ba686cfc1f54bf38435fac55c86
Jorge Romero <romeroqj@gmail.com>
2014-10-15 18:56:18 -0600

    add selected/unselected state to indicators

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,17 @@
 app.controller('CravingController', function($scope) { 
+	$scope.moneyState = "unselected";
+	$scope.timeState = "unselected";
+	$scope.frequencyState = "unselected";
 
+	$scope.select = function(indicator){
+		if (indicator === 'money') {
+			$scope.moneyState = "selected";
+		}
+		if (indicator === 'time') {
+			$scope.timeState = "selected";
+		}
+		if (indicator === 'frequency') {
+			$scope.frequencyState = "selected";
+		}
+	}
 });
